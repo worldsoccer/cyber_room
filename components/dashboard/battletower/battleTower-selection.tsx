ed4585b6bd9f346cd054e069467a8c357a6fc343
worldsoccer <113328164+worldsoccer@users.noreply.github.com>
2025-01-20 19:32:35 +0900

    done

--- components/dashboard/battletower/battleTower-selection.tsx
+++ components/dashboard/battletower/battleTower-selection.tsx
@@ -158,7 +158,7 @@ export default function BattleTowerSelection({
       <div className="flex items-center justify-between p-4 bg-gray-100 rounded shadow-sm">
         <div>
           <p className="text-gray-700 font-semibold">
-            選択された階層: <span className="text-blue-600">{userLevel}階</span>
+            階層: <span className="text-blue-600">{userLevel}階</span>
           </p>
         </div>
         <div>
